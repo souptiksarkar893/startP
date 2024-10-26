d41c6e7b466a7b5acda102f84dab5fae754ae3f5
Souptik Sarkar <souptiksarkar893@gmail.com>
2024-10-26 09:59:11 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+const userRouter = require('./routes/user.routes')
+
+//routes declaration
+app.use('/api/v1/users', userRouter)
+
 module.exports = app
