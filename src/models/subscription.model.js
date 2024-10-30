03a1f147c0c5b4e50f119b3159624965c0d65e96
Souptik Sarkar <souptiksarkar893@gmail.com>
2024-10-30 10:27:57 +0530

    "Added four new controller functions: changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, and updateCoverImage; updated exports to include new functions."

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const subscriptionSchema = new mongoose.Schema({
+  subscriber: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  channel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model("Subscription", subscriptionSchema);  
